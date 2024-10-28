558be1fc41e1a1eb26e7b210b9bd7eab3b9b4e0e
svivek19 <svivek030503@gmail.com>
2024-10-28 10:14:43 +0530

    input button created with submit logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
+
 const App: React.FC = () => {
+  const [bookName, setBookName] = React.useState<any[]>([]);
+
+  const handleSubmit = () => {
+    console.log(bookName);
+  };
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Book Store</h1>
+
+      <Box component="form" noValidate autoComplete="off">
+        <TextField
+          id="outlined-basic"
+          label="Book name"
+          onChange={(e) => setBookName([e.target.value])}
+          variant="outlined"
+          sx={{ width: "30ch", height: "55px" }}
+        />
+        <Button
+          variant="contained"
+          color="success"
+          onClick={handleSubmit}
+          sx={{ width: "20ch", height: "55px", marginLeft: "10px" }}
+        >
+          Submit
+        </Button>
+      </Box>
     </div>
   );
 };
